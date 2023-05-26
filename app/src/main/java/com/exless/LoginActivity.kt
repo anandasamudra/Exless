e31bae997be3013d8f25a125c6d43afbc93ac166
kevink5444 <kevinkurniawan901@gmail.com>
2023-05-26 17:59:24 +0700

    Login

--- app/src/main/java/com/exless/LoginActivity.kt
+++ app/src/main/java/com/exless/LoginActivity.kt
@@ -2,11 +2,10 @@ package com.exless
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.kelompok4.exless.R
 
-class Register_Activity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        setContentView(R.layout.activity_login)
     }
 }
