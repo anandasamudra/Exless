b5cd654f393955d08253cad2329f4f336af8799d
SpexID <ngurahadipermana15@gmail.com>
2023-06-11 00:36:04 +0800

    Bottom navigation
    Signed-off-by: SpexID <ngurahadipermana15@gmail.com>

--- app/src/main/java/com/exless/view/MainActivity.kt
+++ app/src/main/java/com/exless/view/MainActivity.kt
@@ -3,10 +3,23 @@ package com.exless.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.exless.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//bottom navigation \/\/\/
+//        val satufrag = firstfragment()
+//        val duafrag = secondfragment()
+//        setfragment(satufrag)
+//        findViewById<BottomNavigationView>(R.id.bottomNavigationView).setOnNavigationItemSelectedListener {
+//            when(it.itemId){
+//                R.id.home -> setfragment(satufrag)
+//                R.id.dua -> setfragment(duafrag)
+//            }
+//            true
+//        }
+        // bottom navigation /\/\/\
     }
 }
