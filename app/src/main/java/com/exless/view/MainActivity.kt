c4986b82c99da9005068d90e9abd482613ab2407
SpexID <ngurahadipermana15@gmail.com>
2023-06-14 03:05:02 +0800

    tambah bahan dropdown
    Signed-off-by: SpexID <ngurahadipermana15@gmail.com>

--- app/src/main/java/com/exless/view/MainActivity.kt
+++ app/src/main/java/com/exless/view/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.exless.view
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import com.exless.R
 import com.exless.view.fragment.fragmentbelanja
@@ -11,9 +14,11 @@ import com.exless.view.fragment.fragmentsimpanan
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
 //bottom navigation fragment \/\/\/
         val fraghome = fragmenthome()
         val fragbel = fragmentbelanja()
@@ -38,4 +43,5 @@ class MainActivity : AppCompatActivity() {
             commit()
         }
     // bottom navigation fragment /\/\/\
+
 }
