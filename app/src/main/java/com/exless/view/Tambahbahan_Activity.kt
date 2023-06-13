492b39543ce4440057fb6e579634831044757613
SpexID <ngurahadipermana15@gmail.com>
2023-06-14 04:27:25 +0800

    tambah autocomplete nama bahan
    Signed-off-by: SpexID <ngurahadipermana15@gmail.com>

--- app/src/main/java/com/exless/view/Tambahbahan_Activity.kt
+++ app/src/main/java/com/exless/view/Tambahbahan_Activity.kt
@@ -14,15 +14,20 @@ class Tambahbahan_Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tambahbahan)
-
+        //
         val items = resources.getStringArray(R.array.Jenis_makanan)
         val adapter = ArrayAdapter(this, R.layout.list_dropdown, items)
         val autocomplete = findViewById<AutoCompleteTextView>(R.id.auto_jenismakan)
         autocomplete.setAdapter(adapter)
+        //
         val items2 = resources.getStringArray(R.array.Jenis_penyimpanan)
         val adapter2 = ArrayAdapter(this, R.layout.list_dropdown, items2)
         val autocomplete2 = findViewById<AutoCompleteTextView>(R.id.auto_jenispenyimpanan)
         autocomplete2.setAdapter(adapter2)
-
+        //
+        val items3 = resources.getStringArray(R.array.nama_bahan)
+        val adapter3 = ArrayAdapter(this, R.layout.list_dropdown, items3)
+        val autocomplete3 = findViewById<AutoCompleteTextView>(R.id.auto_namabahan)
+        autocomplete3.setAdapter(adapter3)
     }
 }
