7bca14f7925ecc684e45155553a27e7e396e3c05
kevink5444 <kevinkurniawan901@gmail.com>
2023-06-17 20:45:24 +0700

    tes

--- app/src/main/java/com/exless/view/LoginActivity.kt
+++ app/src/main/java/com/exless/view/LoginActivity.kt
@@ -2,6 +2,7 @@ package com.exless.view
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.WindowManager
 import android.widget.Button
 import android.widget.EditText
@@ -10,7 +11,13 @@ import android.widget.Toast
 import com.exless.R
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowInsetsControllerCompat
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
@@ -20,10 +27,16 @@ class LoginActivity : AppCompatActivity() {
     private lateinit var loginButton: Button
     private lateinit var toRegisterTextView: TextView
     private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var googleSignInClient: GoogleSignInClient
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
         WindowInsetsControllerCompat(window, window.decorView).apply {
             isAppearanceLightStatusBars = true
         }
@@ -35,35 +48,88 @@ class LoginActivity : AppCompatActivity() {
         loginButton = findViewById(R.id.button)
         toRegisterTextView = findViewById(R.id.tv_toregister)
         firebaseAuth = FirebaseAuth.getInstance()
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+        fun signInWithGoogle() {
+            val signInIntent = googleSignInClient.signInIntent
+            startActivityForResult(signInIntent, RC_SIGN_IN)
+        }
 
-        loginButton.setOnClickListener {
-            val email = emailEditText.text.toString()
-            val password = passwordEditText.text.toString()
-
-            if (email.isNotEmpty() && password.isNotEmpty()) {
-                firebaseAuth.signInWithEmailAndPassword(email, password)
-                    .addOnCompleteListener { task ->
+        val buttonGoogle = findViewById<Button>(R.id.logingoogle)
+        buttonGoogle.setOnClickListener {
+            signInWithGoogle()
+            fun firebaseAuthWithGoogle(account: GoogleSignInAccount) {
+                val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+                firebaseAuth.signInWithCredential(credential)
+                    .addOnCompleteListener(this) { task ->
                         if (task.isSuccessful) {
-
-                            // Login berhasil, lakukan tindakan yang diinginkan
-                            Toast.makeText(this, "Login berhasil", Toast.LENGTH_SHORT).show()
-                            // Contoh: Pindah ke halaman utama aplikasi
+                            // Login berhasil
+                            val user = firebaseAuth.currentUser
+                            // Lakukan tindakan selanjutnya dengan pengguna yang berhasil login
                             startActivity(Intent(this, MainActivity::class.java))
                             finish()
                         } else {
-                            // Login gagal, tampilkan pesan error
-                            Toast.makeText(this, "Login gagal: ${task.exception?.message}", Toast.LENGTH_SHORT).show()
+                            // Handle login failure
+                            Log.e(TAG, "Firebase authentication failed", task.exception)
                         }
                     }
-            } else {
-                Toast.makeText(this, "Mohon lengkapi semua field", Toast.LENGTH_SHORT).show()
-            }
-        }
 
-        toRegisterTextView.setOnClickListener {
-            // Pindah ke halaman registrasi
-            startActivity(Intent(this, Register_Activity::class.java))
-            finish()
+            }
+
+            fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+                super.onActivityResult(requestCode, resultCode, data)
+
+                if (requestCode == RC_SIGN_IN) {
+                    val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+                    try {
+                        val account = task.getResult(ApiException::class.java)
+                        if (account != null) {
+                            firebaseAuthWithGoogle(account)
+                        }
+                    } catch (e: ApiException) {
+                        // Handle login failure
+                        Log.e(TAG, "Google sign-in failed", e)
+                    }
+                }
+            }
+
+
+            loginButton.setOnClickListener {
+                val email = emailEditText.text.toString()
+                val password = passwordEditText.text.toString()
+
+                if (email.isNotEmpty() && password.isNotEmpty()) {
+                    firebaseAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener { task ->
+                            if (task.isSuccessful) {
+
+                                // Login berhasil, lakukan tindakan yang diinginkan
+                                Toast.makeText(this, "Login berhasil", Toast.LENGTH_SHORT).show()
+                                // Contoh: Pindah ke halaman utama aplikasi
+                                startActivity(Intent(this, MainActivity::class.java))
+                                finish()
+                            } else {
+                                // Login gagal, tampilkan pesan error
+                                Toast.makeText(
+                                    this,
+                                    "Login gagal: ${task.exception?.message}",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
+                        }
+                } else {
+                    Toast.makeText(this, "Mohon lengkapi semua field", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            toRegisterTextView.setOnClickListener {
+                // Pindah ke halaman registrasi
+                startActivity(Intent(this, Register_Activity::class.java))
+                finish()
+            }
         }
     }
 }
