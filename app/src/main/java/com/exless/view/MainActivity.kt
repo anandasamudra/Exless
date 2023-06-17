c110031baf25c7c591d5d2f3461db01517caead3
SpexID <ngurahadipermana15@gmail.com>
2023-06-17 23:42:20 +0800

    merge fix
    Signed-off-by: SpexID <ngurahadipermana15@gmail.com>

--- app/src/main/java/com/exless/view/MainActivity.kt
+++ app/src/main/java/com/exless/view/MainActivity.kt
@@ -18,7 +18,6 @@ import com.exless.view.fragment.fragmentkomunitas
 import com.exless.view.fragment.fragmentsimpanan
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
