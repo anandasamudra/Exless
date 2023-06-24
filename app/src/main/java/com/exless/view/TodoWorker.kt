2e6415f54e51e40007a757791e1ea54dabfb2ad8
SpexID <ngurahadipermana15@gmail.com>
2023-06-24 20:33:21 +0800

    adding profile and its intent
    Signed-off-by: SpexID <ngurahadipermana15@gmail.com>

---
+++ app/src/main/java/com/exless/view/TodoWorker.kt
@@ -0,0 +1,15 @@
+package com.exless.view
+
+import android.content.Context
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+
+
+class TodoWorker(val context: Context, val params: WorkerParameters): Worker(context,params) {
+    override fun doWork(): Result {
+        NotificationHelper(context).createNotification(
+            inputData.getString("TITLE").toString(),
+            inputData.getString("MESSAGE").toString())
+        return Result.success()
+    }
+}
